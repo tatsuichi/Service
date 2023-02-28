40f7a8f92efa31b79146176fb802da994042202f
tatsuichi <tatsuri1984@gmail.com>
2023-02-28 22:18:00 +0900

    サービス開始処理/サービス停止処理を追加

--- WorkerService/Worker.cs
+++ WorkerService/Worker.cs
@@ -9,6 +9,27 @@ namespace WorkerService
             _logger = logger;
         }
 
+        /// <summary>
+        /// サービス開始処理
+        /// </summary>
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("サービス開始処理");
+            return base.StartAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// サービス停止処理
+        /// </summary>
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("サービス停止処理");
+            return base.StopAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// サービス定周期処理
+        /// </summary>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
